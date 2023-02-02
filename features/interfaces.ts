a6c1a40d9b177f0b470511045e563598491e4ed1
Devin Raleigh <devinkr@gmail.com>
2023-02-02 16:10:21 -0700

    User interfaces to gatekeep functions

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -68,3 +68,8 @@ const printSummary = (item: Reportable): void => {
 };
 printSummary(oldCivic);
 printSummary(aDrink);
+
+// Use interfaces for gatekeeping
+// Create functions that accept arguments that are typed with interfaces
+// Objects/classes can decide to implement a given interface to work with a function
+
