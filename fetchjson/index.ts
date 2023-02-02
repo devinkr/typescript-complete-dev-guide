215f465f937a806a4644bcae21d4e0bb874a1abd
Devin Raleigh <devinkr@gmail.com>
2023-02-02 13:56:36 -0700

    Add Todo interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,7 +2,17 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
+  // Response.data has propertied of:
+  // id
+  // title
+  // completed
 	const todo = response.data;
   const ID = todo.ID
   const title = todo.Title;
