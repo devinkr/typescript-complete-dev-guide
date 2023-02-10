651f8ef351ed91f1d18e8a45f4e64f90b39eae88
Devin Raleigh <devinkr@gmail.com>
2023-02-10 00:31:23 -0700

    Use static methods to generate prefoncfigured MatchReader and Analysis

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,14 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const CsvFileReader_1 = require("./CsvFileReader");
-const WinsAnalysis_1 = require("./Analyzers/WinsAnalysis");
-const HtmlReport_1 = require("./ReportTargets/HtmlReport");
 const Summary_1 = require("./Summary");
-// Create an Object that satisfies DataReader interface
-const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
-// Create an instance of MatchReader and pass in something satisfying the DataReader interface
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
 matchReader.load();
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Newcastle'), new HtmlReport_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,17 +1,7 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './ReportTargets/ConsoleReport';
-import { WinsAnalysis } from './Analyzers/WinsAnalysis';
-import { HtmlReport } from './ReportTargets/HtmlReport';
 import { Summary } from './Summary';
 
-// Create an Object that satisfies DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in something satisfying the DataReader interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 matchReader.load();
-
-const summary = new Summary(new WinsAnalysis('Newcastle'), new HtmlReport());
-
 summary.buildAndPrintReport(matchReader.matches);
